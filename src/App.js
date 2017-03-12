48710d404a9d1fa24777ed6a3ce1467ead8aac65
Gan Jun Kai <kuhn96@gmail.com>
2017-03-12 14:38:16 +0800

    add state in my component

--- src/App.js
+++ src/App.js
@@ -22,13 +22,21 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        list,
+      };
+  }
+
   render() {
     const helloWorld = 'Welcome to React';
     return (
       <div className="App">
         {
-          list.map(function(item) {
-            return (
+          this.state.list.map(item =>
               <div key={item.objectID}>
                 <span>
                   <a href={item.url}>{item.title}</a>
@@ -37,8 +45,7 @@ class App extends Component {
                 <span>{item.num_comments}</span>
                 <span>{item.points}</span>
               </div>
-            );
-          })
+          )
         }
       </div>
     );
