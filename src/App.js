ef4f31e92a4db6c08e557c9d3871855d34c34df3
Gan Jun Kai <kuhn96@gmail.com>
2017-03-12 18:13:37 +0800

    add search feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const list = [
@@ -21,6 +20,9 @@ const list = [
   }
 ];
 
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props) {
@@ -28,25 +30,38 @@ class App extends Component {
 
       this.state = {
         list,
+        searchTerm: '',
       };
 
       this.onDismiss = this.onDismiss.bind(this);
+      this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
-    const isNotId = item => item.objectID != id;
+    const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
     this.setState({
       list: updatedList
     });
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
+    const { list, searchTerm } = this.state;
     const helloWorld = 'Welcome to React';
     return (
       <div className="App">
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
         {
-          this.state.list.map(item =>
+          list.filter(isSearched(searchTerm)).map(item =>
               <div key={item.objectID}>
                 <span>
                   <a href={item.url}>{item.title}</a>
